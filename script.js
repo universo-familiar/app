84fbf62ba7fd8f43906c0e642ab2e5371f2d8278
Gabriela Catharine Andreatta <gabrielaandreatta2001@gmail.com>
2025-11-21 02:46:14 -0300

    Atualização do site e crianção das páginas HTML

--- script.js
+++ script.js
@@ -47,6 +47,26 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(nextSlide, 6000);
 });
 
+const backToTop = document.getElementById("backToTop");
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    backToTop.style.display = "flex";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  item.querySelector(".faq-question").addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
 
 
